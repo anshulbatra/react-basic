1f2d5c1f88c859096e36c73915217c3f3bdfbfc0
Anshul Batra <anshul_batra99@ymail.com>
2020-04-23 19:43:59 +0530

    App parent and header, footer, mainComponent Children

---
+++ 06_parent_child_component/MainContent.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function MainContent() {
+    return (
+        <div>
+            <h3>Hello World</h3>
+            <ol>
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+            </ol>
+        </div>
+    )
+}
+
+export default MainContent
